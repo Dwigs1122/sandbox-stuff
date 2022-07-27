595490dceede0bd52ba9d44b73a0c8f2c4393c78
sarah@gfcengineering.com <sarah@gfcengineering.com>
2022-07-27 10:48:04 -0600

    showed how you would maintain state in controller - wait for the cnc to finish one task before performing another

--- cnc_module.py
+++ cnc_module.py
@@ -1,13 +1,33 @@
 from time import sleep
 
-
 def cnc_main(channel, module_instruction):
-    # Use a breakpoint in the code line below to debug your script.
+    print("[CNCModule.cnc_main] CNC module here. I am operating in {}. I was just instructed to do: {}".format(channel,module_instruction), flush=True)
 
-    print("[cnc_main] CNC module here. I am operating in {}. I was just instructed to do: {}".format(channel,module_instruction))
-    sleep(1)
-    print("[cnc_main] CNC module again, I am in {}. just finished doing: {}".format(channel, module_instruction))
+    # here is where the logic belongs for deciding which method/function to call based on info originating from plc:
+    if module_instruction == 'Open_Door':
+        open_door()
+
+    elif module_instruction == 'Close_Door':
+        close_door()
+
+
+    print("[CNCModule.cnc_main] CNC module again, I am in {}. just finished doing: {}".format(channel, module_instruction), flush=True)
+
     results = "done. success. etc. or maybe i return metrics."
     result_list = [channel, 'cnc_main',module_instruction, results]
+
+
+
     return result_list
+
+def open_door():
+    print("[CNCModule.open_door] starting open_door method", flush=True)
+    sleep(4)
+    print("[CNCModule.open_door] done with opening door.", flush=True)
+
+
+def close_door():
+    print("[CNCModule.close_door] starting close_door method", flush=True)
+    sleep(1)
+    print("[CNCModule.close_door] done with closing door", flush=True)
 
--- using_multi_processing.py
+++ using_multi_processing.py
@@ -6,29 +6,43 @@ from asyncua import Client, Node, ua
 import cnc_module
 import robot_module
 
+global robot_is_busy
+global cnc_is_busy
+
+
 
 def callback_from_any_module(result_list):
+    global robot_is_busy, cnc_is_busy
     x=result_list
     #channel, cell_module,module_instruction, results_passed_in  = [result_list[i] for i in (range(0,2))]
     channel, cell_module, module_instruction, results_passed_in  = [result_list[i] for i in (range(0,4))]
 
     a = channel
     b = cell_module
-    print("[callback_from_any_module] The {} module called in channel: {} returned: {} . this is where we would write back to PLC on correct channel.".format(cell_module, channel, results_passed_in))
+    print("[callback_from_any_module] The {} module called in channel: {} returned: {} . this is where we would write back to PLC on correct channel.".format(cell_module, channel, results_passed_in), flush=True)
 
     ## write to opcua var to blindly communicate the results from this module
     ## some function or inline code to do this
 
+    ## free up the hardware to perform a new task
+    if cell_module == 'robot_main':
+        robot_is_busy = False
+    elif cell_module == 'cnc_main':
+        cnc_is_busy = False
 
 
 def master_commander_in_chief():
-    # commenting out, since this is just a demonstration, won't actually connect to opc client
-    #async with Client(url='http://127 - etc...') as client:
+        global robot_is_busy, cnc_is_busy
+        robot_is_busy = False
+        cnc_is_busy = False
+        # commenting out, since this is just a demonstration, won't actually connect to opc client
+        #async with Client(url='http://127 - etc...') as client:
+
 
         ## poll the opcua server perpetually
         while True:
             try:
-                print('[master_commander_in_chief] Waiting for instructions...')
+                print('[master_commander_in_chief] Waiting for instructions...', flush=True)
                 ## pool for 4, reset every time you are in while true - all pooled processes will complete prior to returning to the beginning of this while, again
                 pool=Pool()
                 results = []
@@ -50,8 +64,8 @@ def master_commander_in_chief():
                     elif channel == 'CHANNEL_3':
                         opcua_var_content = 'CNC|Open_Door'
                     elif channel == 'CHANNEL_4':
-                        # simulate nothing coming thru on this channel from PLC
-                        pass
+                        opcua_var_content = 'CNC|Close_Door'
+
 
 
                     ## if we have content coming thru on channel x, do something...
@@ -61,37 +75,48 @@ def master_commander_in_chief():
                         ## hardware = module
                         cell_module,module_instruction = opcua_var_content.split('|')
 
-                        print("[master_commander_in_chief] {} was just instructed by PLC to do: {} on channel: {}".format(cell_module,module_instruction,channel))
+                        print("[master_commander_in_chief] {} was just instructed by PLC to do: {} on channel: {}".format(cell_module,module_instruction,channel), flush=True)
 
                         # pass the instructions to the appropriate module. also pass channel to maintain "state," as it were
                         if cell_module == 'ROBOT':
+                            # is the robot currently performing a previous task? if so wait
+                            while robot_is_busy:
+                                print("[master_commander_in_chief] waiting for robot to be done with previous task...", flush=True)
+                                sleep(.5)
+
+                            robot_is_busy = True
                             args = [channel,module_instruction]
                             r = pool.apply_async(robot_module.robot_main, args, callback=callback_from_any_module)
                             results.append(r)
-                            print("[master_commander_in_chief] just issued instruction to {} to do: {} on channel: {}".format(cell_module,module_instruction,channel))
+                            print("[master_commander_in_chief] just issued instruction to {} to do: {} on channel: {}".format(cell_module,module_instruction,channel), flush=True)
 
                         elif  cell_module == 'CNC':
-                            a=1
+                            # is the cnc currently performing a previous task? if so wait
+                            while cnc_is_busy:
+                                print("[master_commander_in_chief] waiting for cnc to be done with previous task...", flush=True)
+                                sleep(.5)
+
+                            cnc_is_busy = True
                             args = [channel,module_instruction]
                             r = pool.apply_async(cnc_module.cnc_main, args, callback=callback_from_any_module)
                             results.append(r)
-                            print("[master_commander_in_chief] just issued instruction to {} to do: {} on channel: {}".format(cell_module,module_instruction,channel))
+                            print("[master_commander_in_chief] just issued instruction to {} to do: {} on channel: {}".format(cell_module,module_instruction,channel), flush=True)
 
                 for r in results:
                     r.wait()
 
-                print("[master_commander_in_chief] done with for loop, sleeping at the bottom of while loop")
-                print("......................................................\n\n\n")
+                print("[master_commander_in_chief] done with for loop, sleeping at the bottom of while loop", flush=True)
+                print("......................................................\n\n\n", flush=True)
                 sleep(2)
             except Exception as e:
-                print("bad. exception. {}".format(e))
+                print("bad. exception. {}".format(e), flush=True)
 
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # todo: demonstrate mid-task callbacks back to this controller module (for communicating back from child module to PLC, via opc, all within this 'using_multi_processing.py' controller ...
+    # todo: demonstrate mid-task callbacks back to this controller module (for communicating back from child module to PLC, via opc, all within this 'using_multi_processing_orig.py' controller ...
     master_commander_in_chief()
 
 
